49ea5b64e5ab0e8ba352f588f7587f0b397a96b9
Paragon Initiative Enterprises <security@paragonie.com>
2016-06-06 15:49:53 -0400

    Version 0.3.0
    Add sendUnencrypted(), support decryption/verification with the server's key. Bundled an example key -ONLY FOR UNIT TESTING PURPOSES-

--- src/GPGMailer.php
+++ src/GPGMailer.php
@@ -24,16 +24,25 @@ class GPGMailer
     protected $options;
 
     /**
+     * @var string
+     */
+    protected $serverKeyFingerprint;
+
+    /**
      * GPGMailer constructor.
      * @param TransportInterface $transport
-     * @param array $options
+     * @param array $options For Crypt_GPG
      */
     public function __construct(
         TransportInterface $transport,
-        array $options = []
+        array $options = [],
+        string $serverKey = ''
     ) {
         $this->mailer = $transport;
         $this->options = $options;
+        if (!empty($serverKey)) {
+            $this->serverKeyFingerprint = $this->import($serverKey);
+        }
     }
 
     /**
@@ -47,6 +56,24 @@ class GPGMailer
         $gnupg = new \Crypt_GPG($this->options);
         $gnupg->addEncryptKey($fingerprint);
         return $gnupg->exportPublicKey($fingerprint, true);
+    }
+
+    /**
+     * Encrypt the body of an email.
+     *
+     * @param Message $message
+     * @return Message
+     */
+    public function decrypt(Message $message): Message
+    {
+        $gnupg = new \Crypt_GPG($this->options);
+
+        $gnupg->addDecryptKey($this->serverKeyFingerprint);
+
+        // Replace the message with its encrypted counterpart
+        $decrypted = $gnupg->decrypt($message->getBodyText());
+
+        return $message->setBody($decrypted);
     }
 
     /**
@@ -68,16 +95,49 @@ class GPGMailer
     }
 
     /**
+     * Encrypt the body of an email.
+     *
+     * @param Message $message
+     * @param string $fingerprint
+     * @return Message
+     * @throws \Exception
+     */
+    public function encryptAndSign(Message $message, string $fingerprint): Message
+    {
+        if (!$this->serverKeyFingerprint) {
+            throw new \Exception('No signing key provided');
+        }
+        $gnupg = new \Crypt_GPG($this->options);
+        $gnupg->addEncryptKey($fingerprint);
+        $gnupg->addSignKey($this->serverKeyFingerprint);
+
+        // Replace the message with its encrypted counterpart
+        $encrypted = $gnupg->encryptAndSign($message->getBodyText(), true);
+
+        return $message->setBody($encrypted);
+    }
+
+    /**
+     * Return the stored Transport.
+     *
+     * @return TransportInterface
+     */
+    public function getTransport(): TransportInterface
+    {
+        return $this->getTransport();
+    }
+
+    /**
      * Import a public key, return the fingerprint
      *
-     * @param string $publicKey
-     * @return string
+     * @param string $gpgKey An ASCII armored public key
+     * @return string The GPG fingerprint for this key
      */
-    public function import(string $publicKey): string
+    public function import(string $gpgKey): string
     {
         try {
             $gnupg = new \Crypt_GPG($this->options);
-            $data = $gnupg->importKey($publicKey);
+            $data = $gnupg->importKey($gpgKey);
             return $data['fingerprint'];
         } catch (\Crypt_GPG_NoDataException $ex) {
             return '';
@@ -94,8 +154,103 @@ class GPGMailer
      */
     public function send(Message $message, string $fingerprint)
     {
+        if ($this->serverKeyFingerprint) {
+            $this->mailer->send(
+                // Encrypted, signed
+                $this->encryptAndSign($message, $fingerprint)
+            );
+        } else {
+            $this->mailer->send(
+                // Encrypted, unsigned
+                $this->encrypt($message, $fingerprint)
+            );
+        }
+    }
+
+    /**
+     * Encrypt then email a message
+     *
+     * @param Message $message The message data
+     * @param bool $force      Send even if we don't have a private key?
+     */
+    public function sendUnencrypted(Message $message, bool $force = false)
+    {
+        if (!$this->serverKeyFingerprint) {
+            if ($force) {
+                // Unencrypted, unsigned
+                $message->setBody($message->getBodyText());
+            }
+            return;
+        }
         $this->mailer->send(
-            $this->encrypt($message, $fingerprint)
+            // Unencrypted, signed:
+            $this->sign($message)
         );
+    }
+
+    /**
+     * Sets the private key for signing.
+     *
+     * @param string $serverKey
+     * @return GPGMailer
+     */
+    public function setPrivateKey(string $serverKey): self
+    {
+        $this->serverKeyFingerprint = $this->import($serverKey);
+        return $this;
+    }
+
+    /**
+     * Sign a message (but don't encrypt)
+     *
+     * @param Message $message
+     * @return string
+     * @throws \Exception
+     */
+    public function sign(Message $message): Message
+    {
+        if (!$this->serverKeyFingerprint) {
+            throw new \Exception('No signing key provided');
+        }
+        $gnupg = new \Crypt_GPG($this->options);
+        $gnupg->addSignKey($this->serverKeyFingerprint);
+
+        $message->setBody(
+            $gnupg->sign(
+                $message->getBodyText(),
+                \Crypt_GPG::SIGN_MODE_CLEAR,
+                true
+            )
+        );
+        return $message;
+    }
+
+
+    /**
+     * Verify a message
+     *
+     * @param Message $message
+     * @param string $fingerprint
+     * @return string
+     * @throws \Exception
+     */
+    public function verify(Message $message, string $fingerprint): bool
+    {
+        $gnupg = new \Crypt_GPG($this->options);
+        $gnupg->addSignKey($fingerprint);
+
+        /**
+         * @var \Crypt_GPG_Signature[]
+         */
+        $verified = $gnupg->verify($message->getBodyText());
+        foreach ($verified as $sig) {
+            if (false) {
+                $sig = new \Crypt_GPG_Signature();
+            }
+            if ($sig->isValid()) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- tests/EmailTest.php
+++ tests/EmailTest.php
@@ -6,17 +6,43 @@ use \Zend\Mail\Transport\Sendmail;
 class EmailTest extends PHPUnit_Framework_TestCase
 {
     /**
-     * @covers GPGMailer::encrypt()
      * @covers GPGMailer::import()
+     * @covers GPGMailer::export()
+     */
+    public function testImport()
+    {
+        // Instantiate GPGMailer:
+        $gpgMailer = new GPGMailer(
+            new Sendmail(),
+            ['homedir' => '~']
+        );
+
+        $publicKey = file_get_contents(__DIR__ . '/public.key');
+        $fingerprint = $gpgMailer->import($publicKey);
+        $this->assertSame(
+            '61ED8025A6A602BED499590DE0585BBD212C89BF',
+            $fingerprint
+        );
+
+        $exported = $gpgMailer->export($fingerprint);
+        $this->assertSame(
+            $this->stripComment($publicKey),
+            $this->stripComment($exported)
+        );
+    }
+
+    /**
+     * @covers GPGMailer::decrypt()
+     * @covers GPGMailer::encrypt()
      */
     public function testEncryptedMessage()
     {
         // First, create a Zend\Mail message as usual:
+        $plaintext = 'Cleartext for now. Do not worry; this gets encrypted. Don\' actually send this, however.';
+        $plaintext .= \random_bytes(32);
         $message = new Message;
         $message->addTo('nobody@example.com', 'GPGMailer Test Email');
-        $message->setBody(
-            'Cleartext for now. Do not worry; this gets encrypted. Don\' actually send this, however.'
-        );
+        $message->setBody($plaintext);
 
         // Instantiate GPGMailer:
         $gpgMailer = new GPGMailer(
@@ -24,62 +50,62 @@ class EmailTest extends PHPUnit_Framework_TestCase
             ['homedir' => '~']
         );
 
-        $publicKey = '-----BEGIN PGP PUBLIC KEY BLOCK-----
-Version: GnuPG v1
-
-mQENBFUgwRUBCADcIpqNwyYc5UmY/tpx1sF/rQ3knR1YNXYZThzFV+Gmqhp1fDH5qBs9foh1
-xwI6O7knWmQngnf/nBumI3x6xj7PuOdEZUh2FwCG/VWnglW8rKmoHzHAivjiu9SLnPIPAgHS
-Heh2XD7q3Ndm3nenbjAiRFNl2iXcwA2cTQp9Mmfw9vVcw0G0z1o0G3s8cC8ZS6flFySIervv
-fSRWj7A1acI5eE3+AH/qXJRdEJ+9J8OB65p1JMfk6+fWgOB1XZxMpz70S0rW6IX38WDSRhEK
-2fXyZJAJjyt+YGuzjZySNSoQR/V6vNYnsyrNPCJ2i5CgZQxAkyBBcr7koV9RIhPRzct/ABEB
-AAG0IVNlY3VyaXR5IDxzZWN1cml0eUBwYXJhZ29uaWUuY29tPokBOQQTAQIAIwUCVSDBFQIb
-AwcLCQgHAwIBBhUIAgkKCwQWAgMBAh4BAheAAAoJEGuXocKCZATat2YIAIoejNFEQ2c1iaOE
-tSuB7Pn/WLbsDsHNLDKOV+UnfaCjv/vL7D+5NMChFCi2frde/NQb2TsjqmIH+V+XbnJtlrXD
-Vj7yvMVal+Jqjwj7v4eOEWcKVcFZk+9cfUgh7t92T2BMX58RpgZF0IQZ6Z1R3FfC9Ub4X6yk
-W+te1q0/4CoRycniwmlQi6iGSr99LQ5pfJq2Qlmz/luTZ0UX0h575T7dcp2T1sX/zFRk/fHe
-ANWSksipdDBjAXR7NMnYZgw2HghEdFk/xRDY7K1NRWNZBf05WrMHmh6AIVJiWZvI175URxEe
-268hh+wThBhXQHMhFNJM1qPIuzb4WogxM3UUD7mJAhwEEAECAAYFAlUgxJcACgkQRigTqCu8
-gE3Z0g//WqUZSQE5QbtRmAUAoWIr6ug/ytFGe9dZ8F1qBiUsJVAHyKf1bFBZgfC63oVHJNfO
-4qxJ2qGLKKNQy4YNrYBE0enrrsgDTcp3qDENne9VSE4I+bMJIFDMfwd73DfJsF3PgxgkpumN
-Pd7lFSraY9yjdRyC5iz7q4bEEiiZ6rdDLuVOvtnwnTyoduN0ZtpmbT/6I40LOMeP00peq+2n
-OHdggnRtmm/0wVu38GLz8SgYl4Q/IccbKiXnbsPZDvtXCyviFNT+Shve017uJFjQtz9lUGqf
-+w/o2g3By9bGjIDSOvmgY7i0HF2B4GMZK4B7MT1haJ/ObIDKPyqRxrunvUSh38bv6KZc5F+V
-/4u7qz4Wy03rC8HHSJan5yuxUCQXgMIopk5DOcCvKQ6GAq0WPwPWy0EgSPjbjxpHeSx9lgsI
-ERimEGPl8tOnnVh0DlJCEGttAkE+a2e0R572yTRUM50zSct6ZnVYGB4v7hFFD8censaF1/Jm
-ZhpDd73RQZFApdkBiIVpXgQzzRl6mxX05WkWZHjlQigatjUUQWQtOBTCa+9pFGHopTqA12ju
-rLqrQyYMlSfQCyXtc1fQGvedXhTVnBYQ6DXH6hE+uFDj5/iT4WUVCm8ngfnhqH38NoB+RNn2
-F3EBq5RMx0NF5Kzx3XkZvWvNgFXlSlxkDE7GUpLa6me5AQ0EVSDBFQEIALNkpzSuJsHAHh79
-sc0AYWztdUe2MzyofQbbOnOCpWZebYsC3EXU335fIg59k0m6f+O7GmEZzzIv5v0i99GS1R8C
-Jm6FvhGqtH8ZqmOGbc71WdJSiNVE0kpQoJlVzRbig6ZyyjzrggbM1eh5OXOk5pw4+23FFEdw
-7JWU0HJS2o71r1hwp05Zvy21kcUEobz/WWQQyGS0Neo7PJn+9KS6wOxXul/UE0jct/5f7KLM
-dWMJ1VgniQmmhjvkHLPSICteqCI04RfcmMseW9gueHQXeUu1SNIvsWa2MhxjeBej3pDnrZWs
-zKwygF45GO9/v4tkIXNMy5J1AtOyRgQ3IUMqp8EAEQEAAYkBHwQYAQIACQUCVSDBFQIbDAAK
-CRBrl6HCgmQE2jnIB/4/xFz8InpM7eybnBOAir3uGcYfs3DOmaKn7qWVtGzvrKpQPYnVtlU2
-i6Z5UO4c4jDLT/8Xm1UDz3Lxvqt4xCaDwJvBZexU5BMK8l5DvOzH6o6P2L1UDu6BvmPXpVZz
-7/qUhOnyf8VQg/dAtYF4/ax19giNUpI5j5o5mX5w80RxqSXV9NdSL4fdjeG1g/xXv2luhoV5
-3T1bsycI3wjk/x5tV+M2KVhZBvvuOm/zhJjeoLWp0saaESkGXIXqurj6gZoujJvSvzl0n9F9
-VwqMEizDUfrXgtD1siQGhP0sVC6qha+F/SAEJ0jEquM4TfKWWU2S5V5vgPPpIQSYRnhQW4b1
-=Z4m0
------END PGP PUBLIC KEY BLOCK-----';
-
+        $publicKey = file_get_contents(__DIR__ . '/public.key');
         $fingerprint = $gpgMailer->import($publicKey);
-        $exported = $gpgMailer->export($fingerprint);
-        $this->assertSame(
-            $this->stripComment($publicKey),
-            $this->stripComment($exported)
-        );
-        $this->assertSame(
-            '7F52D5C61D1255C731362E826B97A1C2826404DA',
-            $fingerprint
-        );
 
         $encrypted = $gpgMailer->encrypt($message, $fingerprint);
         $body = $encrypted->getBodyText();
         $this->assertTrue(
             \strpos($body, '-----BEGIN PGP MESSAGE-----') !== false
         );
+        $privateKey = file_get_contents(__DIR__ . '/private.key');
+        $gpgMailer->setPrivateKey($privateKey);
+        
+        $decrypted = $gpgMailer->decrypt($encrypted);
+        $this->assertSame(
+            $plaintext,
+            $decrypted->getBodyText()
+        );
     }
 
+    /**
+     * @covers GPGMailer::sign()
+     * @covers GPGMailer::verify()
+     */
+    public function testSignedMessage()
+    {
+        // First, create a Zend\Mail message as usual:
+        $message = new Message;
+        $message->addTo('nobody@example.com', 'GPGMailer Test Email');
+        $message->setBody(
+            'Cleartext for now. We are going to sign this.'
+        );
+
+        $privateKey = file_get_contents(__DIR__ . '/private.key');
+
+        // Instantiate GPGMailer:
+        $gpgMailer = new GPGMailer(
+            new Sendmail(),
+            ['homedir' => '~'],
+            $privateKey
+        );
+
+        $publicKey = file_get_contents(__DIR__ . '/public.key');
+        $signature = $gpgMailer->sign($message);
+        $this->assertTrue(
+            \strpos($signature->getBodyText(), '-----BEGIN PGP SIGNATURE-----') !== false
+        );
+
+        $fingerprint = $gpgMailer->import($publicKey);
+        $this->assertTrue(
+            $gpgMailer->verify($signature, $fingerprint)
+        );
+    }
+
+    /**
+     * @param string $input
+     * @return string
+     */
     protected function stripComment(string $input): string
     {
         $pieces = \explode("\n", $input);
